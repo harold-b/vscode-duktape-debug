1524e46ae2b37d199410b0666e5f7d1d4c54666f
Simpson, Chris <chris.simpson@nuance.com>
2019-10-08 13:33:37 -0400

    Simplified path logic.

--- src/DukDebugger.ts
+++ src/DukDebugger.ts
@@ -2264,7 +2264,7 @@ export class DukDebugSession extends DebugSession
                     continue;
                 }
                 for (const candidateFile of candidate.srcMap._sources) {
-                    if (candidateFile && Path.resolve(Path.dirname(this.normPath(Path.join(this._outDir, candidate.name))), candidateFile) === path)
+                    if (candidateFile && this.normPath(Path.resolve(this._outDir, candidateFile)) === path)
                     {
                         return candidate;
                     }
